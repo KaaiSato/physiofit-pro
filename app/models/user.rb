9093cd2e2f8b2063390dea1496a090b8f27b0f95
KaaiSato <kaai.sato@gmail.com>
2024-08-17 07:33:37 +0900

    Trainerの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
-class Trainer < ApplicationRecord
+class User < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
 
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable, and :omniauthable
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
@@ -18,16 +18,12 @@ class Trainer < ApplicationRecord
     validates :nickname
     validates :email, uniqueness: true
     validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,}+\z/i }
-    validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龠々ー]+\z/ }
-    validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龠々ー]+\z/ }
-    validates :last_name_reading, format: { with: /\A[ぁ-んー]+\z/ }
-    validates :first_name_reading, format: { with: /\A[ぁ-んー]+\z/ }
     validates :age, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
     validates :gender_id
     validates :prefecture_id
     validates :city
-    validates :registration_number, format: { with: /\A\d{6}\z/ }
-    validates :career
     validates :profile
   end
 end
+
+
