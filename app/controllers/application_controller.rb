e1f470c94cbc0a854a8db68632ea9d937dfca393
KaaiSato <kaai.sato@gmail.com>
2024-08-17 07:54:20 +0900

    コントローラーの作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,24 +1,65 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  private
+  protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [
-      :nickname,
-      :profile_image,
-      :last_name,
-      :first_name,
-      :last_name_reading,
-      :first_name_reading,
-      :age,
-      :gender_id,
-      :prefecture_id,
-      :city,
-      :registration_number,
-      :career,
-      :profile,
-    ])
+    if resource_class == User
+      devise_parameter_sanitizer.permit(:sign_up, keys: [
+        :nickname,
+        :email,
+        :age,
+        :gender_id,
+        :prefecture_id,
+        :city,
+        :profile
+      ])
+      devise_parameter_sanitizer.permit(:account_update, keys: [
+        :nickname,
+        :email,
+        :age,
+        :gender_id,
+        :prefecture_id,
+        :city,
+        :profile
+      ])
+    elsif resource_class == Trainer
+      devise_parameter_sanitizer.permit(:sign_up, keys: [
+        :nickname,
+        :email,
+        :last_name,
+        :first_name,
+        :last_name_reading,
+        :first_name_reading,
+        :age,
+        :sex_id,
+        :prefecture_id,
+        :city,
+        :registration_number,
+        :career,
+        :profile,
+        :latitude,
+        :longitude,
+        :service_radius_km
+      ])
+      devise_parameter_sanitizer.permit(:account_update, keys: [
+        :nickname,
+        :email,
+        :last_name,
+        :first_name,
+        :last_name_reading,
+        :first_name_reading,
+        :age,
+        :sex_id,
+        :prefecture_id,
+        :city,
+        :registration_number,
+        :career,
+        :profile,
+        :latitude,
+        :longitude,
+        :service_radius_km
+      ])
+    end
   end
-
 end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,12 @@
+class UsersController < ApplicationController
+  def index
+  
+  end
+
+  private
+  
+  def after_sign_in_path_for(resource)
+    user_root_path
+  end
+
+end
