18ff8dceb26a0019ff0e4804d5c250812a559377
KaaiSato <kaai.sato@gmail.com>
2024-09-02 13:35:51 +0900

    render-build.shの修正

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,23 +2,7 @@
 # exit on error
 set -o errexit
 
-# 正しいRubyバージョンを使用する
-rbenv install -s 3.2.0  # Rubyのバージョンを指定
-
-# Bundlerを最新バージョンに更新
-gem install bundler
-
-# 依存関係のインストール設定を変更
-bundle config set --local deployment 'true'
-bundle config set --local without 'development test'
-
-# プラットフォームを追加
-bundle lock --add-platform x86_64-linux
-
-# 依存関係のインストール
 bundle install
-
-# アセットのプリコンパイルとデータベースマイグレーション
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
 bundle exec rake db:migrate
